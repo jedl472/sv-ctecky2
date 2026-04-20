e240245be53e4f8d686955f21d895656812474d4
jedl472 <jedl472@proton.me>
2026-04-20 14:15:17 +0200

    minor fixes

--- main/main.c
+++ main/main.c
@@ -1,10 +1,15 @@
-#include "input.h"
+#include <driver/gpio.h>
+#include <driver/spi_master.h>
+#include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <stdio.h>
+#include <string.h>
+#include <u8g2.h>
 
-#include "freertos/FreeRTOS.h"
+#include "sdkconfig.h"
+#include "u8g2_esp32_hal.h"
 
+void app_main(void) {
 
-void app_main() {
-  input_setup();
-  
-  
 }
